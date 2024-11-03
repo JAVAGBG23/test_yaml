db1bc0f750da925cb12350fb44f3c574cf7306bf
Helena <helena@klarr.se>
2024-11-03 21:15:08 +0100

    test

--- src/test/java/health/care/booking/BookingApplicationTests.java
+++ src/test/java/health/care/booking/BookingApplicationTests.java
@@ -3,7 +3,7 @@ package health.care.booking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookingApplicationTests {
 
 	@Test
