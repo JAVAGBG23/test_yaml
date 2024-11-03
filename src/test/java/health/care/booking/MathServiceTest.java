5664ff5b3f0149a59917dee42ebb47e20d4474c9
Helena <helena@klarr.se>
2024-11-03 21:04:04 +0100

    test

--- src/test/java/health/care/booking/MathServiceTest.java
+++ src/test/java/health/care/booking/MathServiceTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class MathServiceTest {
 
         @InjectMocks
